ae536328592cf28e0760eb8a3108efae6f2eab79
Calvin McMurray <calvin.r.mcm@gmail.com>
2014-08-01 12:55:34 -0600

    Added functionality so that the output box now displays a list of all of the entries the user has made. Functionality includes code to prevent addition of empty entries and to clear the text field after each time the primary button is clicked.

--- helloworld.js
+++ helloworld.js
@@ -1,13 +1,19 @@
 $(function(){
 	var clicked = false;
 	var mb = $("#MyButton");
-	var ob = $("#OutputBox"); 
- 	var hist;
-	mb.click(function(){
+	var ob = $("#OutputBox");
+	var list = new Array();
+	var originalText = ob.text(); // Used to make sure that the "List of Recent Entries: <br>" line is not lost.
+	var txt = $("#MyField");
+	mb.click(function(){ // The idea is that every time the button is click, it updates a list of entries next to the field on the webpage.
 		mb.text("Clicked!");
 		clicked = true;
-		var txt = $("#MyField").val();
-		ob.text(txt);
+		if(txt.val() != ""){
+			list.push(txt.val());
+		}
+		var toPrint = list.join(", ");
+		ob.text(originalText + toPrint);
+		txt.val("");
 	})
 	.mouseleave(function(){
 		if(clicked){
